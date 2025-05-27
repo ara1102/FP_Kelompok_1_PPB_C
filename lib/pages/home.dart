2ba498fd6e72cd5cbab1fd7d4285fc3fd21eb647
ara1102 <adhirariyanti@gmail.com>
2025-05-27 07:29:19 +0700

    Add default firebase login register and init home page

---
+++ lib/pages/home.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  int _selectedIndex = 0;
+
+  static final List<Widget> _pages = <Widget>[
+    Center(child: Text('Personal Chat', style: TextStyle(fontSize: 24))),
+    Center(child: Text('Group Chat', style: TextStyle(fontSize: 24))),
+    Center(child: Text('Gemini', style: TextStyle(fontSize: 24))),
+    Center(child: Text('Profile', style: TextStyle(fontSize: 24))),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  final List<BottomNavigationBarItem> _navItems = const [
+    BottomNavigationBarItem(
+      icon: Icon(Icons.person),
+      label: 'Personal',
+    ),
+    BottomNavigationBarItem(
+      icon: Icon(Icons.group),
+      label: 'Group',
+    ),
+    BottomNavigationBarItem(
+      icon: Icon(Icons.auto_awesome),
+      label: 'Gemini',
+    ),
+    BottomNavigationBarItem(
+      icon: Icon(Icons.account_circle),
+      label: 'Profile',
+    ),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Chat App'),
+        centerTitle: true,
+      ),
+      body: _pages[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        items: _navItems,
+      ),
+    );
+  }
+  
+}
